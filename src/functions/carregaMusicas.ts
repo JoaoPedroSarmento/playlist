4c1a0ab37688b3f8f4d2068155be29ae423ca70e
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2024-04-30 20:51:26 -0300

    Update carregaMusicas.ts

--- src/functions/carregaMusicas.ts
+++ src/functions/carregaMusicas.ts
@@ -3,6 +3,8 @@ import fazFetch from "./fazFetch";
 
 export default async function carregaMusicas(): Promise<Array<Musica>> {
   try {
+
+       console.log(process.env.NEXT_PUBLIC_CARREGA_MUSICA!);
     const musicas: Array<Musica> = await fazFetch(
       "GET",
       process.env.NEXT_PUBLIC_CARREGA_MUSICA!
