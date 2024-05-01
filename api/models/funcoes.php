f3238dd2890299b6e57db87cadd0ad3665724463
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2024-04-30 21:20:04 -0300

    env

--- api/models/funcoes.php
+++ api/models/funcoes.php
@@ -1,9 +1,15 @@
 <?php
+  $env = $_ENV;
+  $dbName = $env["DB_NAME"];
+  $host = $env["HOST"];
+  $root = $env["ROOT"];
+  $senha = $env["SENHA"];
+
     function getConexao():PDO{
-        $DSN = 'mysql:dbname=bg5twkjfgkncohcxq4oj;host=bg5twkjfgkncohcxq4oj-mysql.services.clever-cloud.com;charset=utf8';
+        $DSN = "mysql:dbname=$dbname;host=$host;charset=utf8";
 
         try{
-            $pdo = new PDO($DSN,'uki0wiingu0endsh','f1J6za2UL7XR0mqVLlhe',[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
+            $pdo = new PDO($DSN,$root,$senha,[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
         }catch(PDOException $error){
             die("ERRO AO CONECTAR COM O BANCO DE DADOS {$error->getMessage()}");
         }
